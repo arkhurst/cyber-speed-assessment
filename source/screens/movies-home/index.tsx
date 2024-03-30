f75efcd5e038f4dd60d7222f42f786b8d7e05539
arkhurst <fiiarkhurst@gmail.com>
2024-03-30 17:10:21 +0000

    added react native vector icons

--- source/screens/movies-home/index.tsx
+++ source/screens/movies-home/index.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
 import {Text, SafeAreaView} from 'react-native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const MoviesHome = () => {
   return (
     <SafeAreaView>
       <Text>Movies Home</Text>
+      <MaterialCommunityIcons name="bell" color={'red'} size={20} />
     </SafeAreaView>
   );
 };
