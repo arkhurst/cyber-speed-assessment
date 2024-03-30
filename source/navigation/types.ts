a04efec091db57a07b1be3843050c22f5af66d50
arkhurst <fiiarkhurst@gmail.com>
2024-03-30 13:57:20 +0000

    added basic navigation flow

---
+++ source/navigation/types.ts
@@ -0,0 +1,4 @@
+export type RootStackParamList = {
+  moviesHome: undefined;
+  movieDetails: {movieId: string};
+};
---
+++ source/screens/movies-home/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import {Text, SafeAreaView} from 'react-native';
+
+const MoviesHome = () => {
+  return (
+    <SafeAreaView>
+      <Text>Movies Home</Text>
+    </SafeAreaView>
+  );
+};
+
+export {MoviesHome};
